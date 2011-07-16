60626ccb0fafe566fd986d28b64ae2abf35b5f2a
Fran Hermoso <franhp@gmail.com>
2011-07-16 21:27:03 +0200

    really added jquery ui

--- includes/style.class.php
+++ includes/style.class.php
@@ -10,6 +10,7 @@ class Style{
 		<link href="'.$settings->siteurl.'/css/style.css" media="screen" rel="stylesheet" type="text/css" />
 		<script src="'.$settings->siteurl.'/js/jquery-1.6.2.min.js" type="text/javascript"></script>
 		<script src="'.$settings->siteurl.'/js/jquery-ui-1.8.14.min.js" type="text/javascript"></script>
+		<link href="'.$settings->siteurl.'/css/jquery-ui-1.8.14.css" media="screen" rel="stylesheet" type="text/css" />
 		<script src="'.$settings->siteurl.'/js/jquery.scrollTo.js" type="text/javascript"></script>
 		<script type="text/javascript" src="'.$settings->siteurl.'/external/ckeditor/ckeditor.js"></script>
 		<script type="text/javascript" src="'.$settings->siteurl.'/external/ckeditor/adapters/jquery.js"></script>
